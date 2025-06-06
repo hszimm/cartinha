78d15286543d0ed30ecb18603c22a1621f4715ac
hszimm <t7389864@gmail.com>
2025-06-06 16:18:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-
-let cartaAtual = 1;
-function mostrarCarta(num) {
-    document.querySelectorAll('.carta').forEach((el, i) => {
-        el.classList.remove('ativa');
-        if (i === num - 1) el.classList.add('ativa');
-    });
-}
-function proximaCarta() {
-    cartaAtual = cartaAtual === 3 ? 1 : cartaAtual + 1;
-    mostrarCarta(cartaAtual);
-}
-function anteriorCarta() {
-    cartaAtual = cartaAtual === 1 ? 3 : cartaAtual - 1;
-    mostrarCarta(cartaAtual);
-}
-document.addEventListener("DOMContentLoaded", () => {
-    mostrarCarta(cartaAtual);
-});
-
-function entrarSite() {
-    document.getElementById("entrada").style.display = "none";
+function verificarSenha() {
+  const senha = document.getElementById("senha").value;
+  if (senha === "160423") {
+    document.getElementById("senha-container").style.display = "none";
     document.getElementById("conteudo").style.display = "block";
-    const audio = document.getElementById("musica");
-    if (audio) {
-        audio.play().catch(() => {});
-    }
+  } else {
+    alert("Senha incorreta!");
+  }
 }
